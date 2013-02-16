a6e354bbb6b3b4aec63d79d3430141717278687c
Kotov Egor I <bimarck555@gmail.com>
2013-02-16 21:16:28 +0200

    Applying HTML\CSS improvements 1) Index.html , bio.php 2) pessenik.css

--- js/pesennik.js
+++ js/pesennik.js
@@ -6,10 +6,54 @@ $(function ($) {
 		$(".artist-photo-top-new").fadeIn(1500);
 	});
 	
-	$(".nav-header").click(function(){
-		$(".nav-header ul>li").toggle('slow');
+	
+// MENU _BEGIN
+	$(".header1").click(function(){
+		$(".header1 ul>li").toggle('slow');
 	});
 	
+	$(".header2").click(function(){
+		$(".header2 ul>li").toggle('slow');
+	});
+	
+	$(".header3").click(function(){
+		$(".header3 ul>li").toggle('slow');
+	});
+	
+	
+	$(".header4").click(function(){
+		$(".header4 ul>li").toggle('slow');
+	});
+// MENU _END
+	
+// NEW HEADINGS _BEGIN	
+	$("#headings-container").css('height',$("#measure_block>div").height()+"px");
+	var counter_menu = 0;
+	var counter_block = 0;
+	var shift = $("#measure_block>div").height();
+	var shift_menu = $("#new-heading-menu>p").height();
+	$(".btn-next-heading").click(function(){
+		//$(".next-heading ul>li").toggle('slow');
+		if (counter_block < 2){		
+			counter_block++;
+			//var temp_position = '-=' +  shift +'px';
+			$("#new-heading-block").animate({'top':   -(counter_block * shift) +'px'},2000,'linear',function(){}); // -=540
+			//$("#new-heading-block").delay(2000).css('top',(-(counter_block-1)*shift)+'px');
+		}else{
+			counter_block = 0;
+			$("#new-heading-block").animate({'top': '0'},2000,function(){}); // +=1080
+		}
+		
+		if (counter_menu < 2){		
+			counter_menu++;
+			//var temp_position_menu = '-=' +  (shift_menu + 10) + 'px';
+			$("#new-heading-menu").animate({'top':   -(counter_menu * shift_menu + 10) + 'px'},2000,'linear',function(){}); // -=30
+		}else{
+			counter_menu = 0;
+			$("#new-heading-menu").animate({'top':'0'},2000,function(){}); // +=60
+		}
+	});
+// NEW HEADINGS _END	
 	
 	$(".artist-photo-top-new").mouseover(function(){
 	 timer.stop();
---
+++ php/pages/bio.php
@@ -0,0 +1,251 @@
+
+  <div class="main-container" style="margin:20px;">
+	  <div  id="short_bio">
+		<div class="photo"><img src="img/artist.jpg" alt="artist" /></div>
+		<div class="span7" id="short_bio_table" >
+			<span class="vote-stars">
+				<span class="current-rating" style="width:30%;">  </span>	
+			</span>
+			<span style="border:1px solid #541;width:182; height: 18px; float:right;">
+				<!-- margin for element 2px , w:22 h:16-->
+				<img src="" width="22" height="16" alt="google" />
+				<img src="" width="22" height="16" alt="vk" />
+				<img src="" width="22" height="16" alt="face" />
+				<img src="" width="22" height="16" alt="mail.ru" />
+				<img src="" width="22" height="16" alt="tweeter" />
+				<img src="" width="22" height="16" alt="livejournal" />
+				<img src="" width="22" height="16" alt="yandex" />
+			</span>
+			<table class="bio-table" cellpadding="5" cellspacing="3">
+				<tr><td>Полное имя</td> <td>: Ани Лорак</td></tr>
+				<tr><td>Даты</td> <td>: 01.01.1990 - 10.10.2010</td></tr>
+				<tr><td>Страны</td> <td>: Украина</td></tr>
+			</table>
+			
+			<div style="float:right;">
+				<div style="display:inline;">
+					<a href="#">Песни</a> 
+					<a href="#">Альбомы</a>
+				</div>
+				
+				<div style="display:inline;">
+					<span style="border:1px solid #541;width:182; height: 25px; ">
+						<!-- margin for element 2px , w:22 h:16-->
+						<img src="" width="22" height="16" alt="google" />
+						<img src="" width="22" height="16" alt="vk" />
+						<img src="" width="22" height="16" alt="face" />
+						<img src="" width="22" height="16" alt="mail.ru" />
+						<img src="" width="22" height="16" alt="tweeter" />
+						<img src="" width="22" height="16" alt="livejournal" />
+						<img src="" width="22" height="16" alt="yandex" />
+					</span>
+				</div>
+			</div>
+	</div>
+	</div>
+	<div class="bio-content">
+		<div class="bio-section">	  
+			<span class="bio-section-title" > Биография </span>
+			<div class="bio-section-text">
+			Глобальный контекст виден всем программным модулям и определяет общую языковую среду конфигурации.
+			Локальный контекст модуля образуется тем конкретным местом конфигу¬рации задачи, для которого использован программный модуль. Локальный контекст виден только конкретному программному модулю и определяет для модуля набор непосредственно доступных модулю значений агрегатных типов данных, их атрибутов и методов (см. «Виды программных модулей»). Однако, контекст модуля можно передать как объект в виде параметра при вызове процедур и функций (см. «Передача локального контекста программного модуля в качестве параметра»). Кроме того, контекст модуля определяет тот набор мето¬дов, которые доступны только в данном контексте (см. «Атрибуты и методы контекста Модуля формы», «Методы контекста Модуля формы элемента справочника» и т. п.). Локальный контекст предназначен для того, чтобы дать воз¬можность управлять частными аспектами поведения задачи, присущими данному модулю.
+			</div>
+		</div>
+		
+		<div  class="bio-section">	  
+			<span class="bio-section-title" > Награды </span>
+			<div class="bio-rewards">
+				<ul>
+					<li>2010
+						<ul>
+						<li>[Событие,Место] - [Название]</li>
+						<li>Первая гланда России, Берибово - Золотая связка из горла Баскова.</li>
+						<li>Пухлое горло стар-пома, Нижний Тагил - Лучший едок сырых яиц.</li>
+						</ul>
+					</li>
+					<li>2011</li>
+					<li>2012</li>
+					<li>2013</li>
+					
+				</ul>
+			</div>
+		</div>
+		<div class="bio-section">	  
+			<span class="bio-section-title" > Интересные факты </span>
+			<div class="bio-section-text">
+				<ul>
+					<li>
+					[Факт]
+					</li>
+					<li>
+					В 12 лет уже разбила все бокалы голосовыми связками, естественно чужими.
+					</li>
+					<li>
+					30-тый ребенок в семье Гирбета и Зирлоки Лораков, не говорила до 13 лет, но пела с 3.
+					</li>
+					<li>
+					Любимое блюдо жареные куринные лапки .
+					</li>
+				</ul>
+			</div>
+		</div>
+		
+		<div class="bio-section">	  
+			<span class="bio-section-title" > О себе </span>
+			<div class="bio-section-text">
+				<ul>
+					<li>[Думаю, что все равно что писать в этом разеделе]</li>
+					<li>Любит ездить верхом на верблюде и плювать им на поклонников.</li>
+					<li>Часто развлекается завязывая глаза перед дошкольными учереждениями и катаясь по кругу.</li>
+				</ul>
+			</div>
+		</div>
+		
+		<div class="bio-section">	  
+			<span class="bio-section-title" > Фото </span>
+			
+			<!--div  class="bio-section-gallery">
+				<a href="#" id="gallery-left-arrow"><</a>
+				<div id="gallery-content"></div>
+				<a href="#" id="gallery-right-arrow">></a>
+			</div-->
+			
+			<!--TEST IMAGE BEGIN-->			
+			<div id="SlideItMoo_outer">	
+					<div class="SlideItMoo_back"><!--slide back button--></div>
+					
+					<!-- container inside used for hiding the elements outisde the view  -->
+					<div id="SlideItMoo_inner" style="width: 950px;">			
+					<!-- actual elements container, usually having a width bigger than the one of SlideItMoo_inner -->
+						<div id="SlideItMoo_items" style="width: 2680px; margin-left: 0px;">
+							<!-- a single element. Duplicate this for as many times you want to hold the individual elements in slider -->
+							
+							<!-- ROW 1-->
+							<div class="SlideItMoo_element">
+								<strong>1: Address</strong>
+								<a href="img/artist.jpg" rel="lightbox[galerie]" target="_blank">
+									<img src="img/artist.jpg" /></a>
+								Nam porta tellus ac urna    
+							</div>							
+							<div class="SlideItMoo_element">
+								<strong>2: Ads</strong>
+								<a href="img/artist.jpg" rel="lightbox[galerie]" target="_blank">
+									<img src="img/artist.jpg" /></a>
+								Praesent pellentesque eros nec nisl    
+							</div>
+							<div class="SlideItMoo_element">
+								<strong>3: Phone</strong>
+								<a href="img/artist.jpg" rel="lightbox[galerie]" target="_blank">
+									<img src="img/artist.jpg" /></a>
+								Vestibulum eleifend scelerisque purus  
+							</div>
+							<div class="SlideItMoo_element">
+								<strong>4: Lock</strong>
+								<a href="img/artist.jpg" rel="lightbox[galerie]" target="_blank">
+									<img src="img/artist.jpg" /></a>
+								 Felis nulla iaculis lacus
+							</div>
+							
+							<div class="SlideItMoo_element">
+								<strong>5: Zoo</strong>
+								<a href="img/artist.jpg" rel="lightbox[galerie]" target="_blank">
+									<img src="img/artist.jpg" /></a>
+								Quisque neque. Donec quam ante, pulvinar vitae
+							</div>
+							
+							<div class="SlideItMoo_element">
+								<strong>6: Donut</strong>
+								<a href="img/artist.jpg" rel="lightbox[galerie]" target="_blank">
+									<img src="img/artist.jpg" /></a>
+								Suspendisse sodales turpis. Aliquam lectus  
+							</div>
+							<!-- ROW 2-->
+							
+							
+							<div class="SlideItMoo_element">
+								<strong>11: Address</strong>
+								<a href="img/artist.jpg" rel="lightbox[galerie]" target="_blank">
+									<img src="img/artist.jpg" /></a>
+								Nam porta tellus ac urna    
+							</div>							
+							<div class="SlideItMoo_element">
+								<strong>22: Ads</strong>
+								<a href="img/artist.jpg" rel="lightbox[galerie]" target="_blank">
+									<img src="img/artist.jpg" /></a>
+								Praesent pellentesque eros nec nisl    
+							</div>
+							<div class="SlideItMoo_element">
+								<strong>33: Phone</strong>
+								<a href="img/artist.jpg" rel="lightbox[galerie]" target="_blank">
+									<img src="img/artist.jpg" /></a>
+								Vestibulum eleifend scelerisque purus  
+							</div>
+							<div class="SlideItMoo_element">
+								<strong>44: Lock</strong>
+								<a href="img/artist.jpg" rel="lightbox[galerie]" target="_blank">
+									<img src="img/artist.jpg" /></a>
+								 Felis nulla iaculis lacus
+							</div>
+							
+							<div class="SlideItMoo_element">
+								<strong>55: Zoo</strong>
+								<a href="img/artist.jpg" rel="lightbox[galerie]" target="_blank">
+									<img src="img/artist.jpg" /></a>
+								Quisque neque. Donec quam ante, pulvinar vitae
+							</div>
+							
+							<div class="SlideItMoo_element">
+								<strong>66: Donut</strong>
+								<a href="img/artist.jpg" rel="lightbox[galerie]" target="_blank">
+									<img src="img/artist.jpg" /></a>
+								Suspendisse sodales turpis. Aliquam lectus  
+							</div>
+							
+							<!--END ROWS-->
+							
+							
+						</div>			
+					</div>   
+					
+					<div class="SlideItMoo_forward"><!--slide forward button--></div>
+			</div>
+			<!--TEST IMAGE END-->			
+			
+		</div>
+		
+		<!--div class="bio-section">	  
+			<span class="bio-section-title" > Песни/Альбомы </span>
+			
+		</div-->
+	</div>
+			
+	</div>
+
+ 
+	<script language="javascript" type="text/javascript" src="js/jquery.js"></script>
+	<script language="javascript" type="text/javascript">
+	
+		jQuery.noConflict();	
+			 jQuery(function(){
+			 //jQuery('#tabs').tabs();	
+			var sliderMoo = new SlideItMoo({
+								overallContainer: 'SlideItMoo_outer',
+								elementScrolled: 'SlideItMoo_inner',
+								thumbsContainer: 'SlideItMoo_items',		
+								itemsVisible:7,
+								elemsSlide:3,
+								duration:300,
+								itemsSelector: '.SlideItMoo_element',
+								itemWidth: 158,
+								showControls:1,
+								startIndex:5			
+					});
+				
+			 });
+		</script>
+		
+		<link rel="stylesheet" type="text/css" href="css/gallery/horizontal.css" />
+		
+		<script language="javascript" type="text/javascript" src="js/mootools-1.2.1-core.js"></script>
+		<script language="javascript" type="text/javascript" src="js/mootools-1.2-more.js"></script>
+		<script language="javascript" type="text/javascript" src="js/slideitmoo-1.1.js"></script>
