c014174646ad697923a25b64e7d3a70e792069a9
Kotov Egor I <bimarck555@gmail.com>
2013-02-22 08:52:38 +0200

    Added: - Changed:   index.html   1) footer   2) top-new-block template - navigation-bar

--- js/pesennik.js
+++ js/pesennik.js
@@ -1,9 +1,37 @@
 $(function ($) {
 
-	var timer = $.timer(function() {
-		// may be ajax request to server for new photo
+	$("#headings-container").css('height',$("#measure_block>div").height()+"px");
+	var counter_menu = 0;
+	var counter_block = 0;
+	var shift = $("#measure_block>div").height();
+	var shift_menu = $("#new-heading-menu>p").height();
+
+	var timer_hero = $.timer(function() {
+		// may be next step - ajax request to server for new photo
 		$(".artist-photo-top-new").fadeOut(1500);
 		$(".artist-photo-top-new").fadeIn(1500);
+	});
+	
+	var timer_top_new = $.timer(function(){
+		//$(".next-heading ul>li").toggle('slow');
+		if (counter_block < 2){		
+			counter_block++;
+			//var temp_position = '-=' +  shift +'px';
+			$("#new-heading-block").animate({'top':   -(counter_block * shift) +'px'},2000,'linear',function(){}); // -=540
+			//$("#new-heading-block").delay(2000).css('top',(-(counter_block-1)*shift)+'px');
+		}else{
+			counter_block = 0;
+			$("#new-heading-block").animate({'top': '0'},2000,function(){}); // +=1080
+		}
+		
+		if (counter_menu < 2){		
+			counter_menu++;
+			//var temp_position_menu = '-=' +  (shift_menu + 10) + 'px';
+			$("#new-heading-menu").animate({'top':   -(counter_menu * shift_menu + 10) + 'px'},2000,'linear',function(){}); // -=30
+		}else{
+			counter_menu = 0;
+			$("#new-heading-menu").animate({'top':'0'},2000,function(){}); // +=60
+		}
 	});
 	
 	
@@ -27,12 +55,8 @@ $(function ($) {
 // MENU _END
 	
 // NEW HEADINGS _BEGIN	
-	$("#headings-container").css('height',$("#measure_block>div").height()+"px");
-	var counter_menu = 0;
-	var counter_block = 0;
-	var shift = $("#measure_block>div").height();
-	var shift_menu = $("#new-heading-menu>p").height();
-	$(".btn-next-heading").click(function(){
+	
+	/*$(".btn-next-heading").click(function(){
 		//$(".next-heading ul>li").toggle('slow');
 		if (counter_block < 2){		
 			counter_block++;
@@ -52,13 +76,34 @@ $(function ($) {
 			counter_menu = 0;
 			$("#new-heading-menu").animate({'top':'0'},2000,function(){}); // +=60
 		}
-	});
+	});*/
 // NEW HEADINGS _END	
 	
-	$(".artist-photo-top-new").mouseover(function(){
-	 timer.stop();
+	
+	// FOOTER BEGIN
+	$(".footer-arrow-btn").click(function(){
+		$(".counters-block").toggle('slow');
+		return false;
 	});
 	
-	timer.set({ time : 10000, autostart : true });
-	timer.play();
+	// FOOTER END
+	// TIMER 1
+	$(".artist-photo-top-new").mouseover(function(){
+	 timer_hero.stop();
+	});
+	
+	timer_hero.set({ time : 10000, autostart : true });
+	timer_hero.play();
+	
+	
+	// TIMER 2
+	$(".top-new-block").mouseover(function(){
+	 timer_top_new.pause();
+	});
+	$(".top-new-block").mouseleave(function(){
+	 timer_top_new.play();
+	});
+	
+	timer_top_new.set({ time : 15000, autostart : true });
+	timer_top_new.play();
 });
