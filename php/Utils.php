7f87572f0d765ad3e1931b89b4ecf78719b28a71
Sergiy <www@kulibaba.net>
2013-03-08 12:10:26 +0200

    Social networks buttons added

--- php/Utils.php
+++ php/Utils.php
@@ -195,4 +195,18 @@ function getFullDate($date) {
 	$rus_date = str_replace("December", "декабря", $rus_date);
 	return  $rus_date;
 }
+
+function currentPageURL() {
+	$pageURL = 'http';
+	if ($_SERVER["HTTPS"] == "on"){
+		$pageURL .= "s";
+	}
+	$pageURL .= "://";
+	if ($_SERVER["SERVER_PORT"] != "80"){
+		$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+	}else{
+		$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	}
+	return $pageURL;
+}
 ?>
